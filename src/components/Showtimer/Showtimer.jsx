4ce7c1c6ae7e6c910fbbb3b4d850c82cdca4b270
Indraraj Yadav <indra26@Indrarajs-Mac-mini.local>
2024-07-23 22:08:47 +0530

    feat: timer logic and style

--- src/components/Showtimer/Showtimer.jsx
+++ src/components/Showtimer/Showtimer.jsx
@@ -1,9 +1,13 @@
 import classes from "./Showtimer.module.css";
 
-function Showtimer() {
+function Showtimer({ timer }) {
   return (
     <div className={classes.container}>
-      <h1>00:00</h1>
+      <h1 className={classes.h1}>
+        <span> {("0" + Math.floor((timer / 60000) % 60)).slice(-2)}:</span>
+        <span> {("0" + Math.floor((timer / 1000) % 60)).slice(-2)}.</span>
+        <span> {("0" + ((timer / 10) % 100)).slice(-2)}</span>
+      </h1>
     </div>
   );
 }
--- src/components/Stopwatch/Stopwatch.jsx
+++ src/components/Stopwatch/Stopwatch.jsx
@@ -2,20 +2,77 @@ import ShowTimer from "../Showtimer/Showtimer";
 import Button from "../Button/Button";
 
 import classes from "./Stopwatch.module.css";
+import { useEffect, useState } from "react";
 
 function Stopwatch() {
+  const [active, setActive] = useState(false);
+  const [timer, setTimer] = useState(0);
+  const [reset, setReset] = useState(false);
+  const [previousTimer, setPreviousTimer] = useState(0);
+  const [showPreviousTimer, setShowPreviousTimer] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+    if (active) {
+      intervalId = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 10);
+      }, 10);
+    }
+    if (reset) {
+      clearInterval(intervalId);
+      setReset(false);
+    }
+    return () => clearInterval(intervalId);
+  }, [active, reset]);
+
+  const startPauseHandler = () => {
+    setActive(!active);
+  };
+
+  const stopHandler = () => {
+    if (active) {
+      setPreviousTimer(timer);
+      setShowPreviousTimer(true);
+      setActive(false);
+      setTimer(0);
+    }
+  };
+
+  const resetHandler = () => {
+    setTimer(0);
+    setPreviousTimer(0);
+    setActive(false);
+    setReset(true);
+    setShowPreviousTimer(false);
+  };
+
   return (
     <div className={classes.container}>
-      <ShowTimer />
+      <ShowTimer timer={timer} />
+      {showPreviousTimer && (
+        <div className={classes.previousElapsedTime}>
+          <div>Previous Elapsed Time</div>
+          <ShowTimer timer={previousTimer} />
+        </div>
+      )}
       <div className={classes.buttonContainer}>
         <div>
-          <Button color="green">Start</Button>
+          <Button
+            color={active ? "#947505" : "#308952"}
+            onClick={startPauseHandler}
+          >
+            {active ? "Pause" : "Start"}
+          </Button>
         </div>
         <div>
-          <Button color="red">Stop</Button>
+          <Button color="#cf4343" onClick={stopHandler}>
+            Stop
+          </Button>
         </div>
         <div>
-          <Button color="grey">Reset</Button>
+          <Button color="grey" onClick={resetHandler}>
+            Reset
+          </Button>
         </div>
       </div>
     </div>
